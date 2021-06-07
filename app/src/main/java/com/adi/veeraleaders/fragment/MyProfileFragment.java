4e0197a9b44db155316c11a796d0ad1aa99b4482
Serajj <Seraj@9140327455>
2021-06-07 17:03:00 +0530

    By seraj

--- app/src/main/java/com/adi/veeraleaders/fragment/MyProfileFragment.java
+++ app/src/main/java/com/adi/veeraleaders/fragment/MyProfileFragment.java
@@ -9,14 +9,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.adi.veeraleaders.R;
+import com.adi.veeraleaders.model.User;
+import com.adi.veeraleaders.utiles.SessionManager;
 
 
 public class MyProfileFragment extends Fragment {
+
+    SessionManager sessionManager;
+    User user;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+
+        view=inflater.inflate(R.layout.fragment_my_profile, container, false);
+        sessionManager = new SessionManager(getContext());
+        user = sessionManager.getUserDetails(SessionManager.login);
+        return view;
     }
 }
